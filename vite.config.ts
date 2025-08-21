4d9ebece567257ea7f013aebdd0a8703cbc3da4b
laasha <lashakhijakadze01@gmail.com>
2025-08-21 14:11:39 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// განსაზღვრულია GitHub Pages-ზე სწორად გამოქვეყნებისათვის
 export default defineConfig({
   plugins: [react()],
-  base: '/navito-ai/',   // აქ შენი GitHub repo-ს სახელი წერია (navito-ai)
-  build: {
-    outDir: 'dist'
-  }
+  base: '/navito-ai/',
+  build: { outDir: 'dist' }
 })
